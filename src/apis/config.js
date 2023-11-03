466f0ceec05a68f358c138711a43e1a85141e6e5
lizipeng <lizipeng>
2023-11-03 10:15:25 +0800

    1

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import router from '../router'
 
-export const baseUrl = 'http://18.142.149.25'
+export const baseUrl = import.meta.env.VITE_API_BASE_URL
 
 axios.defaults.timeout = 30000
 
